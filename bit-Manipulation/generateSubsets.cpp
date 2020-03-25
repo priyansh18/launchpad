0274a385504e99b9eab40befbc90f5f928415035
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-03-25 13:49:05 +0530

    Generate Subsets

---
+++ bit-Manipulation/generateSubsets.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void filterChars(char a[], int n)
+{
+  int j = 0;
+  while (n > 0)
+  {
+    int last_bit = (n & 1);
+    if (last_bit == 1)
+    {
+      cout << a[j];
+    }
+    j++;
+    n = n >> 1;
+  }
+  cout << endl;
+}
+void printSubsets(char a[])
+{
+  int n = strlen(a);
+  for (int i = 0; i < (1 << n); i++)
+  {
+    filterChars(a, i);
+  }
+}
+int main()
+{
+  char a[100];
+  cin >> a;
+  printSubsets(a);
+}
