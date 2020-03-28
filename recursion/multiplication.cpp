e8ad3321bd892b21b8d8d9b5af92c263abab1b1c
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-03-28 15:17:35 +0530

    Multiplication without *

--- recursion/multiplication.cpp
+++ recursion/multiplication.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int multiplication(int a, int b)
 {
-  if (b ==)
+  if (b == 0)
+  {
+    return 0;
+  }
+  return multiplication(a, b - 1) + a;
+}
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  if ((a > 0 && b < 0) || (a < 0 && b > 0))
+  {
+    cout << -1 * multiplication(abs(a), abs(b));
+  }
+  else
+  {
+    cout << multiplication(abs(a), abs(b));
+  }
 }
