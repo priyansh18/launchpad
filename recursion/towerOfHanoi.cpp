5dbc21262761a46031398a59cd1a9422075e7408
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-03-29 17:55:20 +0530

    Tower of Hanoi

---
+++ recursion/towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void towerOfHanoi(int n, char src, char dest, char helper)
+{
+  if (n == 0)
+  {
+    return;
+  }
+  //1.Move n-1 disk from source to helper
+  towerOfHanoi(n - 1, src, helper, dest);
+  //2.Move nth disk from source to destination
+  cout << "Move " << n << " disk from " << src << " to " << dest << endl;
+  //3 .Move n-1 disk from helper destination
+  towerOfHanoi(n - 1, helper, dest, src);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  towerOfHanoi(n, 'A', 'C', 'B');
+
+  return 0;
+}
