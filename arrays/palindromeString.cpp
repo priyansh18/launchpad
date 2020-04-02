f73bd73907a6e3451ad99177d08ef1548ef3e0e9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-02 13:27:37 +0530

    Palindromic String & Removing Duplicates

---
+++ arrays/palindromeString.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+bool isPalindrome(char a[])
+{
+  int i = 0;
+  int j = strlen(a) - 1;
+  while (i < j)
+  {
+    if (a[i] == a[j])
+    {
+      i++;
+      j--;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main()
+{
+  char a[100];
+  cin.getline(a, 1000);
+  cout << isPalindrome(a);
+
+  return 0;
+}
