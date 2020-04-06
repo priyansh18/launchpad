54ff24d092c0ea4da6f540996717de52243cdfbe
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-06 18:26:20 +0530

    Insertion at Head-Middle-End

--- Data-structures/linkedList/Insertion-at-head.cpp
+++ Data-structures/linkedList/Insertion-at-head.cpp
@@ -19,6 +19,57 @@ void insertionAtHead(node *&head, int data)
   n->next = head;
   head = n;
 }
+int length(node *head)
+{
+  int len = 0;
+  while (head != NULL)
+  {
+    head = head->next;
+    len = len + 1;
+  }
+  return len;
+}
+void insertionAtTail(node *&head, int data)
+{
+  if (head == NULL)
+  {
+    node *n = new node(data);
+    return;
+  }
+  node *tail = head;
+  while (tail->next != NULL)
+  {
+    tail = tail->next;
+  }
+  node *n = new node(data);
+  tail->next = n;
+  return;
+}
+void insertionAtMiddle(node *&head, int data, int p)
+{
+  if (head == NULL || p == 0)
+  {
+    insertionAtHead(head, data);
+  }
+  else if (p > length(head))
+  {
+    insertionAtTail(head, data);
+  }
+  else
+  {
+    int jump = 1;
+    node *temp = head;
+    while (jump <= p - 1)
+    {
+      temp = temp->next;
+      jump += 1;
+    }
+    node *n = new node(data);
+    n->next = temp->next;
+    temp->next = n;
+  }
+}
+
 void print(node *head)
 {
   // node *temp = head;
@@ -34,7 +85,10 @@ int main()
   insertionAtHead(head, 5);
   insertionAtHead(head, 4);
   insertionAtHead(head, 3);
-  insertionAtHead(head, 2);
+  insertionAtHead(head, 1);
+  insertionAtTail(head, 6);
+  insertionAtMiddle(head, 2, 1);
+
   print(head);
 
   return 0;
