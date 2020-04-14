c5065b3245f8c7c9e2ff6bf080216d82108bd1e9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-14 10:38:46 +0530

    Replace Sum in Nodes

---
+++ Data-structures/BinaryTree/replaceSum.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class node
+{
+public:
+  int data;
+  node *left;
+  node *right;
+
+  node(int d)
+  {
+    data = d;
+    left = NULL;
+    right = NULL;
+  }
+};
+node *buildTree()
+{
+  int d;
+  cin >> d;
+
+  if (d == -1)
+  {
+    return NULL;
+  }
+  node *root = new node(d);
+  root->left = buildTree();
+  root->right = buildTree();
+}
+void bFS(node *root)
+{
+  queue<node *> q;
+  q.push(root);
+  q.push(NULL);
+  while (!q.empty())
+  {
+    node *f = q.front();
+    if (q.front() == NULL)
+    {
+      cout << endl;
+      q.pop();
+      if (!q.empty())
+      {
+        q.push(NULL);
+      }
+    }
+    else
+    {
+      cout << f->data << ",";
+      q.pop();
+
+      if (f->left)
+      {
+        q.push(f->left);
+      }
+      if (f->right)
+      {
+        q.push(f->right);
+      }
+    }
+  }
+  return;
+}
+int replaceSum(node *root)
+{
+  if (root == NULL)
+  {
+    return 0;
+  }
+  if (root->left == NULL && root->right == NULL)
+  {
+    return root->data;
+  }
+  int leftSum = replaceSum(root->left);
+  int rightSum = replaceSum(root->right);
+
+  int temp = root->data;
+  root->data = leftSum + rightSum;
+  return temp + root->data;
+}
+int main()
+{
+  node *root = buildTree();
+  bFS(root);
+  cout << endl;
+  replaceSum(root);
+  bFS(root);
+  return 0;
+}
