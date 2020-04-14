1c798eaf63855c48e89cf925c33a47eb60b5be72
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-14 09:47:33 +0530

    Diameter of Tree

---
+++ Data-structures/BinaryTree/diametreOfTree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+class node
+{
+public:
+  int data;
+  node *left;
+  node *right;
+
+  node(int d)
+  {
+    data = d;
+    left = NULL;
+    right = NULL;
+  }
+};
+node *buildTree()
+{
+  int d;
+  cin >> d;
+
+  if (d == -1)
+  {
+    return NULL;
+  }
+  node *root = new node(d);
+  root->left = buildTree();
+  root->right = buildTree();
+}
+int height(node *root)
+{
+  if (root == NULL)
+  {
+    return 0;
+  }
+  int ls = height(root->left);
+  int rs = height(root->right);
+  return max(ls, rs) + 1;
+}
+int diameter(node *root)
+{
+  if (root == NULL)
+  {
+    return 0;
+  }
+  int h1 = height(root->left);
+  int h2 = height(root->right);
+  int op1 = h1 + h2;
+  int op2 = diameter(root->left);
+  int op3 = diameter(root->right);
+  return max(op1, max(op2, op3));
+}
+int main()
+{
+  //complexity O(n^2)
+  node *root = buildTree();
+  cout << "Diameter of tree: " << diameter(root) << endl;
+  return 0;
+}
