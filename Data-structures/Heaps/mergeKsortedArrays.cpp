08a3417b6e3df49e80ee7540046ef7c70f67f8c9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-19 14:57:54 +0530

    Merge K sorted Array

---
+++ Data-structures/Heaps/mergeKsortedArrays.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, pair<int, int>> ppi;
+
+vector<int> mergeKArrays(vector<vector<int>> arr)
+{
+  vector<int> output;
+
+  priority_queue<ppi, vector<ppi>, greater<ppi>> pq;
+
+  for (int i = 0; i < arr.size(); i++)
+    pq.push({arr[i][0], {i, 0}});
+
+  while (pq.empty() == false)
+  {
+    ppi curr = pq.top();
+    pq.pop();
+
+    int i = curr.second.first;
+    int j = curr.second.second;
+
+    output.push_back(curr.first);
+
+    // The next element belongs to same array as
+    // current.
+    if (j + 1 < arr[i].size())
+      pq.push({arr[i][j + 1], {i, j + 1}});
+  }
+
+  return output;
+}
+
+// Driver program to test above functions
+int main()
+{
+  // Change n at the top to change number
+  // of elements in an array
+  vector<vector<int>> arr{{2, 6, 12},
+                          {1, 9},
+                          {23, 34, 90, 2000}};
+
+  vector<int> output = mergeKArrays(arr);
+
+  cout << "Merged array is " << endl;
+  for (auto x : output)
+    cout << x << " ";
+
+  return 0;
+}
