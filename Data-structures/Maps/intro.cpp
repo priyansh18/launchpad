b0c0c736a7cd11ce5d1425deb54a1aa09345cbe4
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-20 18:05:04 +0530

    Introduction to Maps

---
+++ Data-structures/Maps/intro.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+int main()
+{
+  map<string, int> m;
+  // 1.Insert
+  m.insert(make_pair("Mango", 100));
+
+  pair<string, int> p;
+  p.first = "Apple";
+  p.second = 200;
+
+  m.insert(p);
+
+  m["Banana"] = 20;
+  // Update
+  m["Banana"] = 40;
+
+  m["Litchi"] = 400;
+
+  m["Pineapple"] = 240;
+
+  m["Kiwi"] = 240;
+
+  for (auto it = m.begin(); it != m.end(); it++)
+  {
+    cout << it->first << "----" << it->second;
+    cout << endl;
+  }
+  string fruit;
+  cout << "Erase: ";
+  cin >> fruit;
+  m.erase(fruit);
+  for (auto it = m.begin(); it != m.end(); it++)
+  {
+    cout << it->first << "----" << it->second;
+    cout << endl;
+  }
+
+  //Erase
+
+  string fruit1;
+  cout << "Search: ";
+  cin >> fruit1;
+  auto it = m.find(fruit1);
+  if (it != m.end())
+  {
+    cout << "price of " << fruit1 << " is " << m[fruit1] << endl;
+  }
+  else
+  {
+    cout << "Fruit not present";
+  }
+  return 0;
+}
