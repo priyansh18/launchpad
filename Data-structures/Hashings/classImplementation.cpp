e0d90d4c07f145fa98ab2ea62934e56926c55f2f
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-20 11:51:13 +0530

    Insertion

--- Data-structures/Hashings/classImplementation.cpp
+++ Data-structures/Hashings/classImplementation.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
-template <typename T>
 class Node
 {
 public:
   string key;
-  T value;
-  Node<T> *next;
+  int value;
+  Node *next;
   //Constructor
-  Node(string key, T val)
+  Node(string key, int val)
   {
     this->key = key;
     this->value = val;
@@ -22,42 +22,73 @@ public:
       delete next;
     }
   }
-} template <typename T>
+};
 class Hashtable
 {
-  Node<T> **table; //pointing ro array of pointers
+  Node **table; //pointing ro array of pointers
   int current_size;
   int table_size;
+
+  int hashFunction(string key)
+  {
+    int index = 0;
+    int p = 1;
+    for (int j = 0; j < key.length(); j++)
+    {
+      index += (key[j] * p) % table_size;
+      index = index % table_size;
+      p = (p * 27) % table_size;
+    }
+    return index;
+  }
 
 public:
   Hashtable(int ts = 7)
   {
     table_size = ts;
-    table = new Node<T> *[table_size];
+    table = new Node *[table_size];
     current_size = 0;
-    for (int i = 0li < table_size; i++)
+    for (int i = 0; i < table_size; i++)
     {
       table[i] = NULL;
     }
   }
 
-  void insert(string key, T value)
+  void insert(string key, int value)
   {
-    //...
+    int index = hashFunction(key);
+    Node *n = new Node(key, value);
+    n->next = table[index];
+    table[index] = n;
+    current_size++;
   }
-  T search(string key)
+  void print()
   {
-    //...
+    for (int i = 0; i < table_size; i++)
+    {
+      cout << "Bucket" << i << "-->";
+      Node *temp = table[i];
+      while (temp != NULL)
+      {
+        cout << temp->key << " ->";
+        temp = temp->next;
+      }
+      cout<<endl;
+    }
   }
-  void delete (string key)
-  {
-    //...
-  }
-}
+};
 
 int
 main()
 {
+  Hashtable price_menu;
+  price_menu.insert("BurgerPizza", 150);
+  price_menu.insert("Burger", 120);
+  price_menu.insert("coke", 20);
+  price_menu.insert("Noodles", 25);
+  price_menu.insert("shake", 40);
+
+  price_menu.print();
 
   return 0;
 }
