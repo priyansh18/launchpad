b0cab4bebb20fe70ebc9a4bde4222d2c154877e8
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-27 12:15:37 +0530

    Ladders top down approach

---
+++ Dynamic-Programming/Top-Down/Ladders.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int ladders(int n, int k, int dp[])
+{
+  //Base case
+  if (n == 0)
+  {
+    return 1;
+  }
+  //lookup
+  if (dp[n] != 0)
+  {
+    return dp[n];
+  }
+  int ways = 0;
+  for (int i = 1; i <= k; i++)
+  {
+    if (n - i >= 0)
+      ways += ladders(n - i, k, dp);
+  }
+  return ways;
+}
+
+int main()
+{
+  int n, k;
+  cin >> n >> k;
+  int dp[100] = {0};
+  cout << ladders(n, k, dp) << " ways" << endl;
+  return 0;
+}
