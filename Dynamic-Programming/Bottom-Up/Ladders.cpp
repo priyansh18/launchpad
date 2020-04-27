9fec6157840e2d029b32265e4015718e2f32bb69
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-04-27 12:36:28 +0530

    Ladders problem time optimised

--- Dynamic-Programming/Bottom-Up/Ladders.cpp
+++ Dynamic-Programming/Bottom-Up/Ladders.cpp
@@ -19,6 +19,20 @@ int ladders(int n, int k)
   }
   return dp[n];
 }
+int laddersOptimised(int n, int k)
+{
+  int dp[100] = {0};
+  dp[0] = dp[1] = 1;
+  for (int i = 2; i <= k; i++)
+  {
+    dp[i] = 2 * dp[i - 1];
+  }
+  for (int i = k + 1; i <= n; i++)
+  {
+    dp[i] = 2 * dp[i - 1] - dp[i - k - 1];
+  }
+  return dp[n];
+}
 
 int main()
 {
@@ -27,5 +41,8 @@ int main()
   int n, k;
   cin >> n >> k;
   cout << ladders(n, k) << " ways" << endl;
+  //time complexity O(n)
+  cout << laddersOptimised(n, k) << " ways" << endl;
+
   return 0;
 }
